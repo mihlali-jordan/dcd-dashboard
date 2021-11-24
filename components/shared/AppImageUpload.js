369196e0a498852e081ba2b608b27cccf91f8efb
Mihlali_Jordan <biz@jordanmihlali.dev>
2021-11-24 04:15:17 +0200

    Add error handling for image file upload

--- components/shared/AppImageUpload.js
+++ components/shared/AppImageUpload.js
@@ -5,14 +5,32 @@ import ImageUploading from 'react-images-uploading'
 // Icons
 import { PhotographIcon } from '@heroicons/react/outline'
 
+// Hooks
+import { useCustomToast } from '../../lib/hooks/useCustomToast.js'
+
 export default function AppImageUpload({ value, onChange }) {
+  const toast = useCustomToast()
+
+  const handleErrors = () => {
+    toast({
+      title: 'File limit is 300kb. Please upload a smaller file',
+      status: 'error',
+    })
+  }
+
   return (
-    <ImageUploading value={value} onChange={onChange} dataURLKey="data_url">
+    <ImageUploading
+      value={value}
+      onChange={onChange}
+      dataURLKey="data_url"
+      acceptType={['jpg', 'png']}
+      maxFileSize={300000}
+      onError={() => handleErrors()}
+    >
       {({ imageList, onImageUpload, onImageRemove, onImageUpdate }) => (
-        <button
+        <div
           onClick={imageList.length === 0 ? onImageUpload : () => {}}
-          type="button"
-          className="text-gray-500 relative block w-full flex flex-col items-center border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="text-gray-500 cursor-pointer relative block w-full flex flex-col items-center border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           {imageList.length === 0 ? (
             <React.Fragment>
@@ -45,7 +63,7 @@ export default function AppImageUpload({ value, onChange }) {
               ))}
             </React.Fragment>
           )}
-        </button>
+        </div>
       )}
     </ImageUploading>
   )
